3efaea5300a7cc62a86b3fddd536115a686b5a47
zp <260825893@qq.com>
2019-11-22 16:04:36 +0800

    change info
    change

--- App.js
+++ App.js
@@ -9,61 +9,20 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, ScrollView,View} from 'react-native';
 import {createStackNavigator, createSwitchNavigator, createDrawerNavigator, createAppContainer} from "react-navigation";
-import {ThemeProvider, Header, Icon} from 'react-native-elements';
 import AuthLoadingScreen from './src/AuthLoadingScreen';
 import AuthScreen from './src/AuthScreen';
-import MenuScreen from './src/MenuScreen';
-import HomeScreen from './src/HomeScreen';
-import SettingScreen from './src/SettingScreen';
-import HelpScreen from './src/HelpScreen';
 import DetailStack from './src/DetailStack';
-import MainHeader from './src/components/MainHeader';
 
-
-import { Button, Toast } from '@ant-design/react-native';
-import {ElementsTheme} from './src/theme';
-import WebViewPageContainer from "./src/WebViewPageContainer";
-import BasePageContainer from "./src/components/BasePageContainer";
 import {observer,Provider} from 'mobx-react'
 import store from './src/model/Store'
 // 应用界面组
-const MyDrawerNavigator =createDrawerNavigator({
-  Home: {
-    screen: HomeScreen,
 
-  },
-  Settings: {
-    screen: SettingScreen,
-
-  },
-  Help: {
-    screen: HelpScreen,
-
-  },
-}, {
-  drawerPosition: 'left',
-
-  edgeWidth: 150,
-  contentComponent: props => (<MenuScreen {...props}/>),
-});
 
 const MainStack = createStackNavigator({
-  Home: {
-    screen: MyDrawerNavigator,
-  },
-  Detail: {
-    screen: DetailStack,
-  },
-    WebViewPageContainer:{
-     screen:WebViewPageContainer
+    Detail: {
+        screen: DetailStack,
     },
-    BasePageContainer:{
-    screen:BasePageContainer
-    }
 
-}, {
-  initialRouteName: 'Home',
-    headerMode: 'none',
 });
 
 const MySwitchNavigator = createSwitchNavigator({
